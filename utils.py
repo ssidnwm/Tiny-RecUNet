c89092e1933dfcbe85b3103d35384a274cde6d2b
YEHWAN CHO <154236661+ChoYehwan@users.noreply.github.com>
2025-10-17 15:55:59 +0900

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,138 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+class Tee(object):
+    def __init__(self, *files):
+        self.files = files
+    def write(self, obj):
+        for f in self.files:
+            f.write(obj)
+            f.flush()
+    def flush(self):
+        for f in self.files:
+            f.flush()
+            
+def log_images(x, y_true, y_pred, channel=1):
+    images = []
+    x_np = x[:, channel].cpu().numpy()
+    y_true_np = y_true[:, 0].cpu().numpy()
+    y_pred_np = y_pred[:, 0].cpu().numpy()
+    for i in range(x_np.shape[0]):
+        image = gray2rgb(np.squeeze(x_np[i]))
+        image = outline(image, y_pred_np[i], color=[255, 0, 0])
+        image = outline(image, y_true_np[i], color=[0, 255, 0])
+        images.append(image)
+    return images
+
+
+def gray2rgb(image):
+    w, h = image.shape
+    image += np.abs(np.min(image))
+    image_max = np.abs(np.max(image))
+    if image_max > 0:
+        image /= image_max
+    ret = np.empty((w, h, 3), dtype=np.uint8)
+    ret[:, :, 2] = ret[:, :, 1] = ret[:, :, 0] = image * 255
+    return ret
+
+
+def outline(image, mask, color):
+    mask = np.round(mask)
+    yy, xx = np.nonzero(mask)
+    for y, x in zip(yy, xx):
+        if 0.0 < np.mean(mask[max(0, y - 1) : y + 2, max(0, x - 1) : x + 2]) < 1.0:
+            image[max(0, y) : y + 1, max(0, x) : x + 1] = color
+    return image
+
+class DiceLoss(nn.Module):
+
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+        self.smooth = 1.0
+
+    def forward(self, y_pred, y_true):
+        assert y_pred.size() == y_true.size()
+        y_pred = y_pred[:, 0].contiguous().view(-1)
+        y_true = y_true[:, 0].contiguous().view(-1)
+        intersection = (y_pred * y_true).sum()
+        dsc = (2. * intersection + self.smooth) / (
+            y_pred.sum() + y_true.sum() + self.smooth
+        )
+        return 1. - dsc
+
+def dsc(y_pred, y_true):
+    y_pred = np.round(y_pred).astype(int)
+    y_true = np.round(y_true).astype(int)
+    return np.sum(y_pred[y_true == 1]) * 2.0 / (np.sum(y_pred) + np.sum(y_true))
+
+
+def dsc_distribution(volumes):
+    dsc_dict = {}
+    for p in volumes:
+        y_pred = volumes[p][1]
+        y_true = volumes[p][2]
+        dsc_dict[p] = dsc(y_pred, y_true)
+    return dsc_dict
+
+
+def dsc_per_volume(validation_pred, validation_true, patient_slice_index):
+    dsc_list = []
+    num_slices = np.bincount([p[0] for p in patient_slice_index])
+    index = 0
+    for p in range(len(num_slices)):
+        y_pred = np.array(validation_pred[index : index + num_slices[p]])
+        y_true = np.array(validation_true[index : index + num_slices[p]])
+        dsc_list.append(dsc(y_pred, y_true))
+        index += num_slices[p]
+    return dsc_list
+
+
+def postprocess_per_volume(
+    input_list, pred_list, true_list, patient_slice_index, patients
+):
+    volumes = {}
+    num_slices = np.bincount([p[0] for p in patient_slice_index])
+    index = 0
+    for p in range(len(num_slices)):
+        volume_in = np.array(input_list[index : index + num_slices[p]])
+        volume_pred = np.round(
+            np.array(pred_list[index : index + num_slices[p]])
+        ).astype(int)
+        volume_true = np.array(true_list[index : index + num_slices[p]])
+        volumes[patients[p]] = (volume_in, volume_pred, volume_true)
+        index += num_slices[p]
+    return volumes
+
+
+def log_loss_summary(loss, step, prefix=""):
+    print("epoch {} | {}: {}".format(step + 1, prefix + "loss", np.mean(loss)))
+
+def log_scalar_summary(tag, value, step):
+    print("epoch {} | {}: {}".format(step + 1, tag, value))
+
+
+def plot_dsc(dsc_dist):
+    y_positions = np.arange(len(dsc_dist))
+    dsc_dist = sorted(dsc_dist.items(), key=lambda x: x[1])
+    values = [x[1] for x in dsc_dist]
+    labels = [x[0] for x in dsc_dist]
+    labels = ["_".join(l.split("_")[1:-1]) for l in labels]
+    fig = plt.figure(figsize=(12, 8))
+    canvas = FigureCanvasAgg(fig)
+    plt.barh(y_positions, values, align="center", color="skyblue")
+    plt.yticks(y_positions, labels)
+    plt.xticks(np.arange(0.0, 1.0, 0.1))
+    plt.xlim([0.0, 1.0])
+    plt.gca().axvline(np.mean(values), color="tomato", linewidth=2)
+    plt.gca().axvline(np.median(values), color="forestgreen", linewidth=2)
+    plt.xlabel("Dice coefficient", fontsize="x-large")
+    plt.gca().xaxis.grid(color="silver", alpha=0.5, linestyle="--", linewidth=1)
+    plt.tight_layout()
+    canvas.draw()
+    plt.close()
+    s, (width, height) = canvas.print_to_buffer()
+    return np.fromstring(s, np.uint8).reshape((height, width, 4))
+
